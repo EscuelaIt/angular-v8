9548ab68d3b0e45512153b544f7516514eea837f
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2019-09-11 11:50:57 -0500

    🚧 work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+interface User {
+  name: string;
+  age: number;
+  isSingle: boolean;
+  avatar: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,6 +17,21 @@ export class AppComponent {
   name = 'nicolas';
 
   rockbands: string[] = ['nirvana', 'los prisioneros'];
+
+  users: User[] = [
+    {
+      name: 'Nicolas',
+      age: 26,
+      isSingle: false,
+      avatar: 'assets/images/img1.jpg',
+    },
+    {
+      name: 'Julian',
+      age: 30,
+      isSingle: true,
+      avatar: 'assets/images/img2.jpg',
+    }
+  ];
   rockBandName = '';
 
 
