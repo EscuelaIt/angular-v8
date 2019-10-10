a5cba549ba332c3d62bd661933ce47faf8a0973f
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2019-10-10 11:08:59 -0500

    🚧 work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
+
+  deferredPrompt;
+
+  constructor() {
+
+  }
+
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onbeforeinstallprompt(e) {
+    console.log(e);
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    this.deferredPrompt = e;
+  }
+
+  promptUserInstall() {
+    this.deferredPrompt.prompt();
+    this.deferredPrompt.userChoice.then((outcome) => {
+      console.log(outcome); // either "accepted" or "dismissed"
+    });
+  }
 
 }
